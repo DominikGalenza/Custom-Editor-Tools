fac9c58897d20b751cbf0826c57ef79892e6295b
Dominik Gałenza <dominik.galenza@gmail.com>
2024-05-12 14:39:51 +0200

    Updated the gitignore file and created a header for debug functions.

--- Plugins/EditorManager/Source/EditorManager/Private/AssetActions/QuickAssetAction.cpp
+++ Plugins/EditorManager/Source/EditorManager/Private/AssetActions/QuickAssetAction.cpp
@@ -2,11 +2,10 @@
 
 
 #include "AssetActions/QuickAssetAction.h"
+#include "DebugHeader.h"
 
 void UQuickAssetAction::TestFunction()
 {
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Green, TEXT("Working"));
-	}
+	Print(TEXT("Working"), FColor::Green);
+	PrintLog(TEXT("Working"));
 }
